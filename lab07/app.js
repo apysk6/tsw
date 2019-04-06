919486ba08500fa608fe32eee2e97bc7276e9268
Artur Pyśk <a.pysk6@gmail.com>
2019-04-06 17:17:05 +0200

    Finished mastermind server api.

--- lab07/app.js
+++ lab07/app.js
@@ -1,7 +1,6 @@
 /*jshint node: true, esversion: 6 */
 'use strict';
 
-const express = require('express');
 const bodyParser = require('body-parser');
 const errorHandler = require('errorhandler');
 const uuidv1 = require('uuid/v1');
@@ -9,31 +8,20 @@ const uuidv1 = require('uuid/v1');
 const app = require('express')();
 const port = process.env.PORT || 3000;
 
-let uuids = new Map();
-const colors = ['blue', 'yellow', 'red', 'black', 'white', 'green', 'brown', 'purple'];
-
-const createGame = (size, colorCount) => {
-    let selectedColors = [];
-    for (let i = 0; i < colorCount; i++) {
-        selectedColors.push(colors[Math.floor(Math.random()*colors.length)]);
-    }
-
-    let gameBoard = [];
-    for (let i = 0; i < size; i++) {
-        gameBoard.push(selectedColors[Math.floor(Math.random()*colors.length)]);
-    }
-
-    return gameBoard;
-};
+let games = new Map();
 
 app.use(bodyParser.json());
 
 app.post('/game/new', (_req, res) => {
 
     let uuid = uuidv1();   
-    let game = createGame(_req.body.size, _req.body.colors);
+    let game = {
+        "solved": false,
+        "board": createGame(_req.body.size, _req.body.colors),
+        "steps": _req.body.steps
+    }
 
-    uuids.set(uuid, game);
+    games.set(uuid, game);
 
     res.json( {
         game: uuid,
@@ -43,19 +31,86 @@ app.post('/game/new', (_req, res) => {
     });
   });
 
+
 app.post('/game/move', (_req, res) => {
+    let uuid = _req.body.game;
+    let move = _req.body.move;
+
+    let currentGame = games.get(uuid);
+
+    console.log(currentGame.board  + " " + move);
+    let moveResult = moveRating(moveToMap(currentGame.board), moveToMap(move));
+
+    if (moveResult.black === currentGame.board.size) {
+        currentGame.solved = true;
+    }
+    else {
+        currentGame.steps -= 1;
+    }
+
     res.json({
-        black: 1,
-        white: 0
+        board: currentGame.board,
+        black: moveResult.black,
+        white: moveResult.white
     })
 });
 
 app.post('/game/status', (_req, res) => {
+    let uuid = _req.body.game;
+    let currentGame = games.get(uuid);
+
     res.json({
-        
+        game: uuid,
+        solved: currentGame.solved
     })
 });
 
 app.listen(port, function () {
     console.log("Serwer nasłuchuje na porcie " + port);
 });
+
+const createGame = (size, colorCount) => {
+    let gameBoard = [];
+
+    for (let i = 0; i < size; i++) {
+        gameBoard.push(Math.floor(Math.random() * colorCount) + 1);
+    }
+
+    return gameBoard;
+};
+
+const moveRating = (kod, ruch)  => {
+    let result = {
+        black: 0,
+        white: 0
+    }
+
+    kod.forEach( (value, index) => { 
+        let ruchSet = ruch.get(index);
+        
+        if (ruchSet !== undefined) {
+            let intersectionBlack = new Set([...value].filter(x => ruchSet.has(x)));
+            result.black += intersectionBlack.size;
+
+            let differenceKod = new Set([...value].filter(x => !intersectionBlack.has(x)));
+            let differenceRuch = new Set([...ruchSet].filter(x => !intersectionBlack.has(x)));
+            result.white += Math.min(differenceKod.size, differenceRuch.size);
+        
+        }
+    } );
+
+    return result;
+}
+
+const moveToMap = (tab) => {
+    let map = new Map();
+    tab.forEach((element, key) => {
+        if (!map.has(element)) {
+            map.set(element, new Set());         
+        }
+
+        map.get(element).add(key);
+    });
+
+    return map;
+}
