04875849fa71418e57ac6c257f73a06a72d8bf82
Artur Pyśk <a.pysk6@gmail.com>
2019-04-18 17:24:02 +0200

    added communication to make move

--- lab07/skrypt.js
+++ lab07/skrypt.js
@@ -4,15 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const newGameForm = document.getElementById('newGame');
     const newGameButton = document.getElementById('newGameButton');
     const moveForm = document.getElementById('moveForm');
-    const gameStatusButton = document.getElementById('gameStatusButton');
     let gameMenu = document.getElementById('gameMenu');
 
     const colors = new Map();
     let currentColor;
+    let currentColorNumber;
 
     const buildUI = () => {
         moveForm.style.display = "none";
-        gameStatusButton.style.display = "none";
     };
 
     const buildColors = () => {
@@ -55,13 +54,14 @@ document.addEventListener('DOMContentLoaded', () => {
                     alert(this.responseText);
             }
         };
+
+        localStorage.setItem("moves", new Map());
     });
 
     const createNewGame = () => {
         newGameForm.style.display = "none";
         newGameButton.style.display = "none";
         moveForm.style.display = "inline-block";
-        gameStatusButton.style.display = "inline-block";
 
         buildColors();
         buildColorsBrackets();
@@ -74,34 +74,80 @@ document.addEventListener('DOMContentLoaded', () => {
         for (let i = 1; i <= currentGameColors; i++) {
             let colorBracket = document.createElement('div');
             colorBracket.style.backgroundColor = colors.get(i);
-
+            colorBracket.setAttribute("data-colorNumber", i);
             colorBracket.addEventListener('click', function (event) {
                 currentColor = event.target.style.backgroundColor;
+                currentColorNumber = event.target.getAttribute("data-colorNumber");
             });
 
             gameMenu.appendChild(colorBracket);
         }
-    }
+    };
 
     const newMove = () => {
+        let move = [];
         let currentGameSize = localStorage.getItem('gameSize');
 
         for (let i = 0; i < currentGameSize; i++) {
             let moveBracket = document.createElement('div');
             moveBracket.addEventListener('click', function (event) {
                 event.target.style.backgroundColor = currentColor;
+                moveBracket.setAttribute("data-colorNumber", currentColorNumber);
             });
+            
             moveForm.appendChild(moveBracket);
         }
 
         let makeMoveButton = document.createElement('button');
         makeMoveButton.innerHTML = "Ruch";
-        makeMoveButton.addEventListener('click', makeMove)
-        moveForm.appendChild(makeMoveButton);
-
-    }
-
-    const makeMove = () => {
         
+        makeMoveButton.addEventListener('click', function (event) {
+            moveForm.removeChild(makeMoveButton);
+
+            let colorBrackets = Array.from(moveForm.children);
+            colorBrackets.forEach((item) => {
+                let currentColorNumber = item.getAttribute("data-colorNumber");
+                move.push(currentColorNumber);
+            });
+
+            makeMoveRequest(move);
+            
+        });
+
+        moveForm.appendChild(makeMoveButton);
+    };
+
+    const makeMoveRequest = (move) => {
+        let movesMap = localStorage.getItem("moves");
+
+        let request = new XMLHttpRequest();
+
+        let requestData = {
+            game: localStorage.getItem("gameId"),
+            move: move
+        };
+
+        console.log(move);
+
+        request.open('POST', 'http://localhost:3000/game/move', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify(requestData));
+        request.onload = function () {
+
+            if (this.status === 200) {
+                let moveResult = JSON.parse(this.responseText);
+
+                let whiteScore = moveResult.white;
+                let blackScore = moveResult.black;
+
+                console.log(whiteScore);
+                console.log(blackScore);
+                console.log(moveResult.board);
+
+                }
+                else {
+                    alert(this.responseText);
+            }
+        };
     }
 });
